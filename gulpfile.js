619b0f1db87bf114f944a02f14f6169dea3d388e
DenisCaeyers <denis.caeyers@gmail.com>
2018-01-31 18:00:35 +0100

    latest

--- gulpfile.js
+++ gulpfile.js
@@ -17,32 +17,45 @@ var autoprefixer = require('autoprefixer');
 // Webserver packages
 var browserSync = require('browser-sync').create();
 
+// HTML Packages
+var render = require('gulp-nunjucks-render');
+
 // Stylesheet Tasks
 gulp.task('sass-dev', function () {
     return gulp.src([
         './assets/dev/scss/*.scss',
-      ])
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./assets/bin/cache')) 
+    ])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./assets/bin/css/cache'))
 });
 
 // Post CSS Task
-gulp.task('post-css',['sass-dev'], function () {
+gulp.task('post-css', ['sass-dev'], function () {
     return gulp.src([
-        './assets/bin/cache/*.css',
-      ])
-      .pipe(postcss([ autoprefixer() ]))
-      .pipe(gulp.dest('./assets/bin'))
-      .pipe(browserSync.stream());      
+        './assets/bin/css/cache/*.css',
+    ])
+        .pipe(postcss([autoprefixer()]))
+        .pipe(gulp.dest('./assets/bin/css'))
+        .pipe(browserSync.stream());
+});
+
+// HTML Tasks
+gulp.task('nunjucks', function () {
+    return gulp.src('assets/dev/html/*.html')
+        .pipe(render({
+            path: ['assets/dev/html/templates']
+        }))
+        .pipe(gulp.dest('assets/bin'))
 });
 
 // Watch Tasks
-gulp.task('watch',['post-css'], function () {
+gulp.task('watch', ['post-css', 'nunjucks'], function () {
     browserSync.init({
         server: "."
     });
     gulp.watch('./assets/dev/scss/**/*.scss', ['post-css']);
-    gulp.watch("./**/*.html").on('change', browserSync.reload);
+    gulp.watch('./assets/dev/html/*.html', ['nunjucks']);
+    gulp.watch("./assets/bin/*.html").on('change', browserSync.reload);
 });
 
 //Default task
